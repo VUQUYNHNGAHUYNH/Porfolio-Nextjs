54209b806ed0a929f959d2a43a6436c4ccb23948
NGA <r0917268@student.thomasmore.be>
2023-04-08 16:49:15 +0200

    change in next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  jsc: {
+    transform: {
+      react: {
+        throwIfNamespace: false,
+      },
+    },
+  },
+};
